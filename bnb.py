01c597b4999677e7b070adcfb3551d00f563c335
endzikri <endzikri@gmail.com>
2020-04-14 08:59:30 -0700

    dodato izracunavanje hromatskog indeksa grafa

--- bnb.py
+++ bnb.py
@@ -1,20 +1,35 @@
 from Edge import Edge
 import random
+from graphviz import Graph
 
-filename = "franklin.txt"
+filename = "herschel.txt"
 graph = list()
 edges = list() 
-colors = [1,2,3]
+colors = list()
 
-def read_input(): #ucitavamo podatke
-	data = open(filename, "r")
-	for line in data:
-		edge, v1, v2 = line.split(",")
-		v1 = int(v1)
-		v2 = int(v2)
-		graph.append([edge,v1,v2])
-
-	return graph
+def read_input():
+    data = open(filename, "r")
+    vs = list() # lista grana sa ponavljanjem
+    for line in data:
+        edge, v1, v2 = line.split(",") # grana i dva cvora na krajevima
+        v1 = int(v1)
+        v2 = int(v2)
+        graph.append([edge, v1, v2])
+        vs.append(v1)
+        vs.append(v2)
+    chromatic_index = 0
+    # nalazenje stepena grafa(chromatic_index):
+    #print(vs)
+    for i in range(len(vs)):
+        counter = 1
+        for j in range(i+1, len(vs)):
+            if vs[i] == vs[j]:
+                counter += 1
+        if chromatic_index < counter:
+            chromatic_index = counter
+    #print(chromatic_index)
+    
+    return graph, chromatic_index
 
 def add_adjacent_edges():
 	for edge in edges:
@@ -31,38 +46,7 @@ def try_to_color(color_to_set, edge_to_color): #pokusavamo da obojimo granu dato
 	edge_to_color.set_color(color_to_set)
 	return True
 
-"""
-Bool optimal = True
-Bool not_colored
 
-for each grana in lista_grana
-	for each boja in lista_boja #imacemo ih onoliko koliki je stepen grafa
-		BnB(grana, boja, optimal)
-			if optimal == True:
-				break
-	if not_colored == True: #Znaci nismo granu uspeli da
-		optimal = False		obojimo ni jednom bojom iz liste boja i
-							 trebala bi nam nova pa to nije optimalno
-
-
-
-def BnB(grana,boja,optimal):
-
-	if grana obojena: #vec je obojena pa izlazimo iz rekurzije
-		return
-
-	colored = try_to_color(grana,boja)
-
-	if colored == False: #Ako ne mozemo da je obojimo datom bojom "boja"
-		not_colored = True # Nije jos uvek obojena, probacemo sledecom bojom
-	  	return
-
-	else: not_colored = False 
-
-	for each k in lista_susednih_grana_trenutne_grane:
-		for each b in lista_boja:
-			BnB(k,b,optimal) #Nastavljamo rekurziju na ostale grane
-"""
 def BnB(edge, color, optimal):
 
 	if edge.color is not None: 
@@ -75,12 +59,20 @@ def BnB(edge, color, optimal):
 		return
 	else:
 		not_colored = False
+		print(edge)
+		#print(edge.color)
 
 	for k in edge.adjacent_edges:
 		for c in colors:
 			BnB(k,c,optimal)
+		#optimal = True
+		
+	optimal = True
 
-graph = read_input()  
+graph, chromatic_index = read_input()
+
+for i in range(1, chromatic_index + 1):
+    colors.append(i)
 
 for item in graph:
 	edges.append(Edge(item[0],item[1],item[2])) #Lista grana (klasa)
@@ -88,7 +80,7 @@ for item in graph:
 add_adjacent_edges()
 
 
-optimal = True
+optimal = False
 not_colored = False
 
 for edge in edges:
@@ -96,11 +88,22 @@ for edge in edges:
 		BnB(edge, color, optimal)
 		if optimal == True:
 			break
+	#optimal = True
 	if not_colored == True:
 		optimal = False
+
 
 
 
 for edge in edges:
 	print(edge.name, edge.color)
 
+#######################3crtanje
+
+g = Graph('G', filename='solution.gv')#, engine='sfdp')
+
+for edge in edges:
+    g.edge(str(edge.v1), str(edge.v2),label = str(edge.color))
+    
+print(g.source)
+g.view() 
